23eb3f0a2c28b0c91930aead5229c6c06d3f2a82
benggrae <kbh0581@gmail.com>
2023-12-05 16:37:29 +0900

    init: 프로젝트 기본 셋팅

---
+++ src/main/java/org/Idiot/yesslave/YesSlaveApplication.java
@@ -0,0 +1,13 @@
+package org.Idiot.yesslave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesSlaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YesSlaveApplication.class, args);
+    }
+
+}
