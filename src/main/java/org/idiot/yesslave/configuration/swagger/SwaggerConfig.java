4bf3b6efcd63b1c8d7971187458852801af061c2
Zinzo <42106799+zinzoddari@users.noreply.github.com>
2023-12-10 01:13:41 +0900

    feat: YSL-3 swagger 설정 추가 (#1)
    * chore: 패키지명 수정
    
    * feat: swagger 설정 추가
    
    ---------
    
    Co-authored-by: beng9re <kbh0581@gmail.com>

---
+++ src/main/java/org/idiot/yesslave/configuration/swagger/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package org.idiot.yesslave.configuration.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI serverApiConfig() {
+        return new OpenAPI()
+                .info(new Info().title("YES SALVE API")
+                        .description("YES SLAVE API SWAGGER UI입니다."));
+    }
+}
