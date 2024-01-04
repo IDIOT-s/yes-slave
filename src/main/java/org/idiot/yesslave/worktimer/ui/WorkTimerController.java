f92a354ad7e2e4a0f4e4d123f26577499c046324
zinzoddari <zinzoddari@gmail.com>
2024-01-05 00:46:41 +0900

    feat: 타이머 등록하는 API 추가

--- src/main/java/org/idiot/yesslave/worktimer/ui/WorkTimerController.java
+++ src/main/java/org/idiot/yesslave/worktimer/ui/WorkTimerController.java
@@ -1,12 +1,31 @@
 package org.idiot.yesslave.worktimer.ui;
 
 import lombok.RequiredArgsConstructor;
+import org.idiot.yesslave.worktimer.application.WorkTimerService;
+import org.idiot.yesslave.worktimer.domain.RandomCodeGenerator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.time.LocalDateTime;
+
+@RequestMapping("/timer")
 @RestController
 @RequiredArgsConstructor
 class WorkTimerController {
+    private final WorkTimerService workTimerService;
 
+    @PostMapping
+    public ResponseEntity<Void> timerCreate() {
+        Long response = workTimerService.registerTimer(LocalDateTime.now(), new RandomCodeGenerator());
 
-
+        return ResponseEntity.created(ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(response)
+                        .toUri())
+                .build();
+    }
 }
