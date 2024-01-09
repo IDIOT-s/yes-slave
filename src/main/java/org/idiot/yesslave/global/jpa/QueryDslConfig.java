57e390af9d0d29a675d956a149d5f6c60dccefde
Zinzo <zinzoddari@gmail.com>
2024-01-09 12:14:48 +0900

    refactor: @PresistenceContext를 생성자 주입으로 변경

--- src/main/java/org/idiot/yesslave/global/jpa/QueryDslConfig.java
+++ src/main/java/org/idiot/yesslave/global/jpa/QueryDslConfig.java
@@ -1,16 +1,16 @@
 package org.idiot.yesslave.global.jpa;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 @Configuration
+@RequiredArgsConstructor
 public class QueryDslConfig {
-    @PersistenceContext
-    private EntityManager entityManager;
+
+    private final EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
