edc14194fbf8ca173a866dcf2e33cf01cc6b7d22
zinzoddari <zinzoddari@gmail.com>
2024-01-13 16:12:57 +0900

    test: @SpringBootTest 삭제

--- src/test/java/org/idiot/yesslave/worktimer/ui/WorkTimerControllerTest.java
+++ src/test/java/org/idiot/yesslave/worktimer/ui/WorkTimerControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -27,9 +28,9 @@ import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-@AutoConfigureMockMvc
+
+@WebMvcTest
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class WorkTimerControllerTest {
 
     @Autowired
