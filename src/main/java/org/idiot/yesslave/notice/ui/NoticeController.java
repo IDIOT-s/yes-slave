0ad8f4926a2f545dc64c7767b734892e5fd37f59
JHwan96 <dlwnghks00@gmail.com>
2024-02-19 05:18:42 +0900

    refactor: 공지사항 삭제 반환값 void로 변경

--- src/main/java/org/idiot/yesslave/notice/ui/NoticeController.java
+++ src/main/java/org/idiot/yesslave/notice/ui/NoticeController.java
@@ -2,7 +2,10 @@ package org.idiot.yesslave.notice.ui;
 
 import lombok.RequiredArgsConstructor;
 import org.idiot.yesslave.notice.application.NoticeService;
-import org.idiot.yesslave.notice.dto.*;
+import org.idiot.yesslave.notice.dto.NoticeFindResponse;
+import org.idiot.yesslave.notice.dto.NoticeSaveRequest;
+import org.idiot.yesslave.notice.dto.NoticeUpdateRequest;
+import org.idiot.yesslave.notice.dto.NoticeUpdateResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -54,8 +57,7 @@ public class NoticeController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<NoticeDeleteResponse> deleteNotice(@PathVariable Long id) {
-        NoticeDeleteResponse deleteNotice = noticeService.deleteNotice(id);
-        return ResponseEntity.ok(deleteNotice);
+    public void deleteNotice(@PathVariable Long id) {
+        noticeService.deleteNotice(id);
     }
 }
