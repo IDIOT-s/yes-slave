ab4e8b45b7da572cefa0a81205ad851cb0c042a3
JHwan96 <dlwnghks00@gmail.com>
2024-02-28 04:36:39 +0900

    feat: Controller 조회 관련 test 추가

--- src/test/java/org/idiot/yesslave/notice/ui/NoticeControllerTest.java
+++ src/test/java/org/idiot/yesslave/notice/ui/NoticeControllerTest.java
@@ -3,13 +3,16 @@ package org.idiot.yesslave.notice.ui;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.idiot.yesslave.notice.application.NoticeService;
 import org.idiot.yesslave.notice.dto.NoticeSaveRequest;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -20,13 +23,16 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 @SpringBootTest
 @AutoConfigureMockMvc
 class NoticeControllerTest {
-    @MockBean
+    @Autowired
     NoticeService noticeService;
     @Autowired
     private MockMvc mockMvc;
     final String baseUrl = "/notice";
     final String title = "test title";
     final String content = "test content";
+    final long id = 1L;
+    final String newTitle = "new title";
+    final String newContent = "new content";
 
     @Autowired
     private WebApplicationContext ctx;
@@ -41,7 +47,7 @@ class NoticeControllerTest {
 
     @Nested
     @DisplayName("공지를 생성할 때")
-    class noticeCreate {
+    class NoticeCreate {
         @Test
         @DisplayName("성공합니다")
         void createNotice() throws Exception {
@@ -73,6 +79,48 @@ class NoticeControllerTest {
                                     .accept(MediaType.APPLICATION_JSON))
                     .andExpect(MockMvcResultMatchers.status().isBadRequest());
 
+        }
+    }
+
+    @Nested
+    @DisplayName("공지를 조회할 때")
+    class NoticeFind {
+        @Test
+        @DisplayName("전체 조회에 성공합니다")
+        void findAllNotice() throws Exception {
+            NoticeSaveRequest request = NoticeSaveRequest.builder()
+                    .title(title)
+                    .content(content)
+                    .build();
+            noticeService.registerNotice(request);
+
+            MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(baseUrl))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andReturn();
+            System.out.println("@@@@" + mvcResult.getResponse().getContentAsString());
+        }
+
+        @Test
+        @DisplayName("단일 조회에 성공합니다")
+        void findNoticeSuccess() throws Exception {
+            NoticeSaveRequest request = NoticeSaveRequest.builder()
+                    .title(title)
+                    .content(content)
+                    .build();
+            noticeService.registerNotice(request);
+
+            MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + String.format("/%d", id)))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andReturn();
+            System.out.println(mvcResult.getResponse().getContentAsString());
+        }
+
+        @Test
+        @DisplayName("단일 조회에 실패합니다")
+        void findNoticeFail() throws Exception {
+            mockMvc.perform(
+                            MockMvcRequestBuilders.get(baseUrl + String.format("/%d", id)))
+                    .andExpect(MockMvcResultMatchers.status().is4xxClientError());
         }
     }
 
