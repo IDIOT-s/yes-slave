9cd69d8627cf884830115b99f57e8ee3216f7612
JHwan96 <dlwnghks00@gmail.com>
2024-03-04 19:29:46 +0900

    feat: service의 수정 test 추가

--- src/test/java/org/idiot/yesslave/notice/application/NoticeServiceTest.java
+++ src/test/java/org/idiot/yesslave/notice/application/NoticeServiceTest.java
@@ -6,6 +6,7 @@ import org.idiot.yesslave.global.exception.NotFoundException;
 import org.idiot.yesslave.notice.domain.Notice;
 import org.idiot.yesslave.notice.dto.NoticeFindResponse;
 import org.idiot.yesslave.notice.dto.NoticeSaveRequest;
+import org.idiot.yesslave.notice.dto.NoticeUpdateRequest;
 import org.idiot.yesslave.notice.repository.NoticeRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -34,9 +35,13 @@ class NoticeServiceTest {
             .title(title)
             .content(content)
             .build();
+    final NoticeUpdateRequest updateRequest = NoticeUpdateRequest.builder()
+            .title(newTitle)
+            .content(newContent)
+            .build();
 
     @Nested
-    @DisplayName("공지 저장할 때")
+    @DisplayName("공지를 저장할 때")
     class NoticeRegister {
         @Test
         @DisplayName("정상적으로 저장합니다.")
@@ -103,11 +108,44 @@ class NoticeServiceTest {
         @DisplayName("id에 맞는 데이터가 없어 조회에 실패합니다")
         void findNoticeFailByNotFound() {
             // given
-            Notice saveNotice = noticeRepository.save(Notice.createNotice(noticeSaveRequest));
+            noticeRepository.save(Notice.createNotice(noticeSaveRequest));
 
             // expected
             Assertions.assertThatThrownBy(() -> noticeService.findNotice(failId))
                     .isInstanceOf(NotFoundException.class);
         }
     }
+
+    @Nested
+    @DisplayName("공지를 수정할 때")
+    class NoticeUpdate {
+        @Test
+        @DisplayName("수정에 성공합니다")
+        void updateNoticeSuccess() {
+            // given
+            Notice save = noticeRepository.save(Notice.createNotice(noticeSaveRequest));
+
+            // when
+            noticeService.updateNotice(save.getId(), updateRequest);
+            Notice updatedNotice = noticeRepository.findById(save.getId()).get();
+
+            // then
+            SoftAssertions softAssertions = new SoftAssertions();
+            softAssertions.assertThat(updatedNotice.getTitle()).isEqualTo(newTitle);
+            softAssertions.assertThat(updatedNotice.getContent()).isEqualTo(newContent);
+            softAssertions.assertAll();
+        }
+
+        @Test
+        @DisplayName("id에 맞는 데이터가 없어 수정에 실패합니다")
+        void updateNoticeFailById() {
+            // given
+            noticeRepository.save(Notice.createNotice(noticeSaveRequest));
+
+            // expected
+            Assertions.assertThatThrownBy(() -> noticeService.updateNotice(failId, updateRequest))
+                    .isInstanceOf(NotFoundException.class);
+        }
+    }
+
 }
